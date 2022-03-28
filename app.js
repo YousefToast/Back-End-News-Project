50163494f7c9173d3b2dac9b47196a126f79d8a2
YousefToast <yousef.abobaker98@hotmail.com>
2022-03-28 12:15:31 +0100

    completed nc-ticket #2 with added changes

--- app.js
+++ app.js
@@ -7,22 +7,6 @@ app.use(express.json());
 app.get("/api/topics", getTopics);
 
 app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
-});
-
-app.use((err, req, res, next) => {
-  if ((err.status = "22P02")) {
-    res.status(400).send({ msg: "Invalid Input" });
-  } else {
-    next(err);
-  }
-});
-
-app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send({ msg: "Internal Service Error" });
 });
