1db91e54a6dfde456138ae9cdc13d3204ee7a098
YousefToast <yousef.abobaker98@hotmail.com>
2022-03-29 12:51:58 +0100

    completed ticket 9

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const {
   getUsers,
   getComments,
   getArticles,
+  postComment,
 } = require("./controllers/app.controllers");
 
 const app = express();
@@ -23,6 +24,8 @@ app.get("/api/articles/:article_id/comments", getComments);
 
 app.get("/api/articles", getArticles);
 
+app.post("/api/articles/:article_id/comments", postComment);
+
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
@@ -34,6 +37,8 @@ app.use((err, req, res, next) => {
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid Input!" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Article does not exist" });
   } else {
     next(err);
   }
