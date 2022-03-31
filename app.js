1743048d14d8b40311ba1e75e0c417cf157294e6
YousefToast <yousef.abobaker98@hotmail.com>
2022-03-31 09:57:57 +0100

    added welcome api endpoint

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const {
   deleteComment,
 } = require("./controllers/app.controllers");
 
+app.get("/", (req, res, next) => {
+  res.status(200).send({ msg: "Welcome to Shrel News" });
+});
+
 const app = express();
 app.use(express.json());
 
