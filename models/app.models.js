3caa8ede2d3aecdafed2f5276f36e854c973305f
YousefToast <yousef.abobaker98@hotmail.com>
2022-03-31 13:46:28 +0100

    compleeted patch comments

--- models/app.models.js
+++ models/app.models.js
@@ -136,3 +136,21 @@ exports.findUsername = (username) => {
       return result.rows[0];
     });
 };
+
+exports.updateCommentVotes = (comment_id, editVotes) => {
+  const { inc_votes } = editVotes;
+  return db
+    .query(
+      `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;`,
+      [inc_votes, comment_id]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `comment ${comment_id} not found`,
+        });
+      }
+      return result.rows[0];
+    });
+};
